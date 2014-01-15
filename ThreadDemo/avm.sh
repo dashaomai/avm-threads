a767b2ceef3110f7246bb7b333fb0d1568bbe4bb
Bob Jiang <dashaomai@gmail.com>
2014-01-15 13:47:57 +0800

    调整为 Mac 系统上可用的项目配置

--- ThreadDemo/avm.sh
+++ ThreadDemo/avm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/bin/wine /Volumes/STORAGE/Work4SuperFlash/avm/avm.exe /Volumes/STORAGE/Work4SuperFlash/ThreadDemo/bin-debug/ThreadDemo.swf
+/usr/local/bin/wine /Volumes/STORAGE/Work4SuperFlash/avm-threads/avm/avm.exe /Volumes/STORAGE/Work4SuperFlash/avm-threads/ThreadDemo/bin-debug/ThreadDemo.swf
